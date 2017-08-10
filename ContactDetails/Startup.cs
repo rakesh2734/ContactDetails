a1446eb1afc4776314fd9b060b286bada6dfe7ee
LAPTOP-CAQF635U\rakesh <rakesh.ihs55@gmail.com>
2017-08-09 19:31:41 -0500

    Initial Commit

---
+++ ContactDetails/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactDetails.Startup))]
+namespace ContactDetails
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
